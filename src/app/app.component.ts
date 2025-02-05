861447eadd802866270060601c7f65e72b2d4fc6
Juan Carlos Solares <jcsolares@pariver.com>
2025-02-05 14:05:09 +0100

    intento de storage, por revisar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonicStorageModule],
 })
 export class AppComponent {
   constructor() {}
